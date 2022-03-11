9ff8bee8a2450bbcd6a39fa7dbba19361810b3f2
wen <hsiaowen0324@gmail.com>
2022-03-11 20:19:18 +0800

    fix bug

--- js/all.js
+++ js/all.js
@@ -1,13 +1,12 @@
 // TODO: 修正 ESLint 錯誤、補上分號、前輩說要改單引號 QQ
-import axios from 'axios';
 
 const url = 'https://hexschool.github.io/js-filter-data/data.json';
 let data;
 const table = document.querySelector('.table-content');
 
-function renderData(Renderdata) {
+function renderData(rdata) {
   let str = '';
-  Renderdata.forEach((b) => {
+  rdata.forEach((b) => {
   // TODO: 改成 ES6 的 Template Literals (字面字串符)
     const content = `<tr><td>${b.作物名稱
     }</td><td>${b.市場名稱
@@ -22,7 +21,7 @@ function renderData(Renderdata) {
   table.innerHTML = str;
 }
 
-axios.get(url)
+this.axios.get(url)
   .then((res) => {
     data = res.data.filter((a) => a.作物名稱);
     // TODO: 之後拆成 renderData 函式
@@ -39,19 +38,7 @@ function filterCategory(e) {
     category = e.target.dataset.category;
     showData = data.filter((i) => i.種類代碼 === category);
     // TODO: 之後拆成 renderData 函式
-    let str = '';
-    showData.forEach((i) => {
-      const content = `<tr><td>${i.作物名稱
-      }</td><td>${i.市場名稱
-      }</td><td>${i.上價
-      }</td><td>${i.中價
-      }</td><td>${i.下價
-      }</td><td>${i.平均價
-      }</td><td>${i.交易量
-      }</td></tr>`;
-      str += content;
-    });
-    table.innerHTML = str;
+    renderData(showData);
   }
 }
 
